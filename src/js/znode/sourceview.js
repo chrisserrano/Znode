3888fef172ebf01808635dbd64c76485eb97e354
Christopher Serrano <chris.serrano@gmail.com>
2011-11-19 13:04:51 -0500

    Removed unnecessary sourceview code

--- src/js/znode/sourceview.js
+++ src/js/znode/sourceview.js
@@ -2,29 +2,4 @@ function SourceView(){
   var win = $(window);
   var view = $("#sourceView");
   
-  // default workspace dimensions
-  var workspaceWidth = 2000;  
-  var workspaceHeight = 2000;
-  
-  var menu = $("#menu");
-  menu.css({"position" : "absolute", "left" : 100, "top" : 0, "z-index" : 5000, "border" : "1px solid gray", "padding" : 0});
-  menu.hide();
-  
-  view.append("<div id='hit' />");
-  hitConnect = $("#hit");
-  hitConnect.css({"position" : "absolute", "left" : 100, "top" : 0, "z-index" : 4000, "border" : "none", 
-                  "width" : 10, "height": 10, "cursor":"pointer", "font-size": "1px"});
-  
-  $("#menu li").hover(function(){
-    $(this).css("background-color", "#cccccc");
-  },
-  function(){
-    $(this).css("background-color", "white");
-  }).click(function(){
-    menu.hide();
-    var dir = $(this).text();
-    connectNode(dir);
-  });
-  // TODO doesn't load...
-  view.load("..\..\sourceFiles\test.html");
 }
