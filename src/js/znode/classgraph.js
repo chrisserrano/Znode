b8d5ba6b94c8f2680373489b23865743b5eaa5c0
Christopher Serrano <chris.serrano@gmail.com>
2011-11-19 14:08:54 -0500

    parenthesis

--- src/js/znode/classgraph.js
+++ src/js/znode/classgraph.js
@@ -648,9 +648,9 @@ function ClassGraph(){
   
   // JSON helper function
   function addSlashes(str) {
+    str = str.replace(/\"/g,'\\"');
     str = str.replace(/\\/g,'\\\\');
     str = str.replace(/\'/g,'\\\'');
-    str = str.replace(/\"/g,'\\"');
     str = str.replace(/\0/g,'\\0');
     str = str.replace(/\n/g,'\\\\n');
     return str;
