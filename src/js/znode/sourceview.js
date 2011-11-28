bce06d928b5083389029e0a3412a4904132ff243
Christopher Serrano <chris.serrano@gmail.com>
2011-11-27 19:25:18 -0500

    More work on source code view

--- src/js/znode/sourceview.js
+++ src/js/znode/sourceview.js
@@ -2,4 +2,13 @@ function SourceView(){
   var win = $(window);
   var view = $("#sourceView");
   
+  $(function() {
+  	$("#sourceBox").load("lorem.html");
+  });
+  $(function() {
+  	$("#sourceBox").next().load("lorem.html");
+  });
+  $(function() {
+  	$("#sourceBox").next().next().load("lorem.html");
+  });
 }
